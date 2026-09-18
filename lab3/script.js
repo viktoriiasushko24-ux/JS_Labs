6e9c39ed917b530a5c67e51c88a07e2d657c6047
viktoriiasushko24-ux <viktoriia.sushko.24@pnu.edu.ua>
2026-09-18 19:23:42 +0300

    Create script.js

---
+++ lab3/script.js
@@ -0,0 +1,33 @@
+(function () {
+  var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Lora", "Paula", "Laura", "Jim"];
+
+  console.log("За першою літерою 'j' / 'J')");
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+  // Додатковий функціонал
+  // Критерій: обчислення суми ASCII-кодів літер імені. 
+  // Якщо сума парна — Good Bye, якщо непарна — Hello.
+  console.log("\nПарність суми ASCII-кодів літер імені");
+  for (var k = 0; k < names.length; k++) {
+    var currentName = names[k];
+    var asciiSum = 0;
+
+    for (var j = 0; j < currentName.length; j++) {
+      asciiSum += currentName.charCodeAt(j);
+    }
+
+    if (asciiSum % 2 === 0) {
+      byeSpeaker.speak(currentName + " (ASCII sum: " + asciiSum + " - even)");
+    } else {
+      helloSpeaker.speak(currentName + " (ASCII sum: " + asciiSum + " - odd)");
+    }
+  }
+})();
